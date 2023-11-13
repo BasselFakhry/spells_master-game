ca8bf014db9f12c27269c4a001ecbfe94398c419
BasselFakhry <115945638+BasselFakhry@users.noreply.github.com>
2023-11-13 03:16:44 +0200

    Update Source.c
    added hard function and fixed the main

--- Source.c
+++ Source.c
@@ -1,48 +1,107 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "Header.h"
-
+#define MAX_DEPTH 3
 bool isWin(char* spell, int* frequency) {
-	int last = sizeof(spell) / sizeof(char);
+	int last = strlen(spell) - 1;
 	int loc = spell[last - 1] - 'a';
-	if (frequency[loc] == 0)
-		return true;
-	else
-		false;
+	return frequency[loc] == 0;
+}
+
+bool isLoss(char* spell, int* frequency, char*** dictionary) {
+	int last = strlen(spell) - 1;
+	int loc = spell[last - 1] - 'a';
+	for (int i = 0; i < frequency[loc]; i++) {
+		int l = strlen(dictionary[loc][i]);
+		int lc = dictionary[loc][i][l - 1] - 'a';
+		if (frequency[lc] == 0)
+			return true;
+	}
+	return false;
+}
+
+int evaluate(char* spell, int* frequency, char*** dictionary) {
+	if (isWin(spell, frequency))
+		return 200;
+	if (isLoss(spell, frequency, dictionary))
+		return -200;
+	if (frequency[spell[strlen(spell) - 1] - 'a'] >= 50)
+		return 0;
+	return 50 - frequency[spell[strlen(spell) - 1] - 'a'];
+}
+
+
+
+
+int* reshape_freq(int* frequency, int i) {
+	int* arr = (int*)malloc(sizeof(int) * 26);
+	for (int j = 0; j < 26; j++) {
+		arr[j] = frequency[j];
+	}
+	arr[i]--;
+	return arr;
+}
+char* hard(char condition, int* freq, char*** dict) {
+	int t = condition - 'a';
+	char* s;
+	int pow[50] = { 0 };
+	for (int i = 0; i < freq[t]; i++) {
+		if (isWin(dict[t][i],freq)) {
+			s = (char*)malloc(sizeof(dict[t][i]));
+			strcpy(s, dict[t][i]);
+			return s;
+		}
+		pow[i] += evaluate(dict[t][i], reshape_freq(freq, t), dict);
+		for (int j = 0; j < reshape_freq(freq, t)[strlen(dict[t][i])-1]; j++) {
+			if (isLoss(dict[strlen(dict[t][i]) - 1][j], reshape_freq(freq, t),dict)) {
+				s = (char*)malloc(sizeof(dict[t][i]));
+				strcpy(s, dict[t][i]);
+				return s;
+			}
+			else
+				pow[i] -= evaluate(dict[strlen(dict[t][i]) - 1][j], reshape_freq(freq, t), dict);
+		}
+	}
+	int max = 0;
+	for (int i = 0; i < freq[t]; i++) {
+		if (pow[i] > pow[max])
+			max = i;
+	}
+	s = (char*)malloc(sizeof(dict[t][max]));
+	strcpy(s, dict[t][max]);
+	return s;
+
 }
 
 
 
 char* easy(char first, char*** map, int* freq) {
-    int max = 0;
-    int target = (int)(first) - 'a';	//index of our letter in the freq array
-    char* result = NULL;
+	int max = 0;
+	int target = (int)(first)-'a';	//index of our letter in the freq array
+	char* result = NULL;
 	int size_of_first = freq[target];	//number of words starting with this letter
 
-    for (int i = 0; i < size_of_first; i++) {	//searching for the letter with the highest frequence to check if we can provide a wrod that ends in this letter to make it easier for the player to choose by giving him the most available choices
-		
-		if(isWin(map[target][i],freq)){
+	for (int i = 0; i < size_of_first; i++) {	//searching for the letter with the highest frequence to check if we can provide a wrod that ends in this letter to make it easier for the player to choose by giving him the most available choices
+
+		if (isWin(map[target][i], freq)) {
 			result = (char*)malloc(sizeof(map[target][i]));
-			strcpy(result,map[target][i]);
+			strcpy(result, map[target][i]);
 			return result;
 		}
 
 
 
-		if(freq[strlen(map[target][i])-1] > max){
-			max = freq[strlen(map[target][i])-1];
+		if (freq[strlen(map[target][i]) - 1] > max) {
+			max = freq[strlen(map[target][i]) - 1];
 			result = (char*)malloc(sizeof(map[target][i]));
-			strcpy(result,map[target][i]);
+			strcpy(result, map[target][i]);
 		}
-    }
-    return result;
-}
-
-
-
-char* medium(char last, char*** dictionary, int *freq){
-	for (int i = 0; i <freq[(int)last - 'a'] ; i++)           //searching if there is a word that if the bot use it will directly win.(ie: the last character of it has a frequency 0)
+	}
+	return result;
+} 
+char* medium(char last, char*** dictionary, int* freq) {
+	for (int i = 0; i < freq[(int)last - 'a']; i++)           //searching if there is a word that if the bot use it will directly win.(ie: the last character of it has a frequency 0)
 	{
-		if (isWin(dictionary[(int) last - 'a'][i], freq))     //now we are using the "isWin" function to see if there exist such a word
+		if (isWin(dictionary[(int)last - 'a'][i], freq))     //now we are using the "isWin" function to see if there exist such a word
 		{
 			return dictionary[(int)last - 'a'][i];            // if yes, then return this word
 		}
@@ -52,19 +111,16 @@ char* medium(char last, char*** dictionary, int *freq){
 
 
 
-char* Mindscape_Mastery(char last, char*** dictionary){}
-
-
 
 
 int main()
-{
-	int frequency[26] = {0};	  // array representing the frequency of the first letter of each word
-	char ***map;				  // Hashmap for spells
-	char ***used;				  // Hasmap for used spells
-	int used_frequency[26] = {0}; // array representing the frequency of the first letter of each use word
+{	
+	int frequency[26] = { 0 };	  // array representing the frequency of the first letter of each word
+	char*** map;				  // Hashmap for spells
+	char*** used;				  // Hasmap for used spells
+	int used_frequency[26] = { 0 }; // array representing the frequency of the first letter of each use word
 								  //    -------------------------- read the spells----------------------------------
-	FILE *file;
+	FILE* file;
 	errno_t err;
 	err = fopen_s(&file, "spells.txt", "r");
 	if (file == NULL)
@@ -74,8 +130,9 @@ int main()
 	}
 	size_t size = 0;
 	fscanf(file, "%zu", &size);					   // storing the number of spells in size
-	map = (char ***)malloc(sizeof(char **) * 26);  // allocating memory on the heaap for the unused spells map
-	used = (char ***)malloc(sizeof(char **) * 26); // allocating memory on the heaap for the used spells map
+	map = (char***)malloc(sizeof(char**) * 26);  // allocating memory on the heaap for the unused spells map
+	used = (char***)malloc(sizeof(char**) * 26); // allocating memory on the heaap for the used spells map
+
 
 	for (int i = 0; i < size; i++)
 	{ // filling the map and the frequency array
@@ -85,14 +142,14 @@ int main()
 		frequency[p]++;
 		if (frequency[p] == 1)
 		{ // if there is no already a word begiining with this letter in the map
-			map[p] = (char **)malloc(sizeof(char *));
+			map[p] = (char**)malloc(sizeof(char*));
 		}
 		else
 		{ // if there already exist a word starting with this letter in the map we reallocate memory
-			map[p] = (char **)realloc(map[p], sizeof(char *) * frequency[p]);
+			map[p] = (char**)realloc(map[p], sizeof(char*) * frequency[p]);
 		}
 
-		map[p][frequency[p] - 1] = (char *)malloc(strlen(word) + 1);
+		map[p][frequency[p] - 1] = (char*)malloc(strlen(word) + 1);
 		strcpy(map[p][frequency[p] - 1], word);
 	}
 	fclose(file);
@@ -103,7 +160,9 @@ int main()
 
 	printf("please enter your name : ");
 	scanf("%s", &p1, 20);
-	
+	int level;
+	printf("Please enter the level you wish to play; 1,2 or 3: ");
+	scanf("%d", &level);
 	printf("These are the spells that you may use during the game: \n");
 	int count = 0;
 	for (int i = 0; i < 26; i++)
@@ -139,21 +198,37 @@ int main()
 	}
 
 	// Start the game
-
+	
 	int start = 0;
 	char condition;
 	do
-	{
-		char w[20];
+	{	
+		
+		char w [20];
 		if (s == 0)
 		{
 			printf("%s chooses: ", p1);
 			scanf("%s", &w);
 		}
 		else
-		{
-			printf("%bot chooses: ");
-			strcpy(w,easy(condition,map));
+		{	
+			printf("bot chooses: ");
+			if (start == 0)
+				strcpy(w, map[0][0]);
+			else{
+				
+				if (level == 1)
+					strcpy(w, easy(condition, map,frequency));
+				else if (level == 2)
+					strcpy(w, medium(condition, map,frequency));
+				else {
+					
+					strcpy(w, hard(condition, frequency, map));
+				}
+					
+				
+			}
+			printf("%s\n", w);
 		}
 
 		// if a player choosed a spell that don't start with last letter of the previous spell
@@ -195,13 +270,13 @@ int main()
 				start++;
 				if (used_frequency[location] == 1)
 				{
-					used[location] = (char **)malloc(sizeof(char *));
+					used[location] = (char**)malloc(sizeof(char*));
 				}
 				else
 				{
-					used[location] = (char **)realloc(used[location], sizeof(char *) * used_frequency[location]);
+					used[location] = (char**)realloc(used[location], sizeof(char*) * used_frequency[location]);
 				}
-				used[location][used_frequency[location] - 1] = (char *)malloc(strlen(w) + 1);
+				used[location][used_frequency[location] - 1] = (char*)malloc(strlen(w) + 1);
 				strcpy(used[location][used_frequency[location] - 1], w);
 				if (s == 0)
 					s = 1;
